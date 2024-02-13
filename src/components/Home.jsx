bb1e21b62b57603558fc7074d8a3b3f43052545a
captain-dread <ricardmi.amz@gmail.com>
2024-02-12 18:55:01 -0800

    wip: Add state modifiers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,12 +17,8 @@ export default function Home({ toggleDarkMode, darkMode }) {
     krakenBloods: 0,
     wins: 0,
     losses: 0,
-    // Collection Of All Pirates That Participates
     pirates: [],
-    // [{poe, commodities, lavishLockers, won, pirates}]
     battles: [],
-    // {poe, commodities, lavishLockers, won, pirates}
-    latestBattle: [],
   });
 
   const [battle, setBattle] = useState({
@@ -37,8 +33,114 @@ export default function Home({ toggleDarkMode, darkMode }) {
   const [showBootyCounter, setShowBootyCounter] = useState(true);
 
   const addBattleResult = (chatLogContent, payPerGreedy) => {
-    const battleResults = getLatestBattleResult(chatLogContent, payPerGreedy);
-    console.log(battleResults);
+    const {
+      wonBattle,
+      poe,
+      commodities,
+      lavishLockers,
+      pirates: newPirates,
+      greedyHitPayCommands,
+      greedyHitsSummary,
+    } = getLatestBattleResult(chatLogContent, payPerGreedy);
+
+    setResults((prevState) => {
+      const updatedPirates = [];
+      let updatedWins = prevState.wins;
+      let updatedLosses = prevState.losses;
+      let updatedPOE = prevState.poe;
+      let updatedCommodities = prevState.commodities;
+      let updatedLavishLockers = prevState.lavishLockers;
+      let updatedBattles = [...prevState.battles];
+
+      newPirates.forEach((newPirate) => {
+        const existingPirateIndex = prevState.pirates.findIndex(
+          (pirate) => pirate.name === newPirate.name
+        );
+        if (existingPirateIndex === -1) {
+          // If it's a new pirate, add them to the pirates array with battles: 1
+          updatedPirates.push({ ...newPirate, battles: 1 });
+        } else {
+          // If it's an existing pirate, update their greedyHits and battles count
+          const existingPirate = prevState.pirates[existingPirateIndex];
+          const updatedGreedyHits =
+            existingPirate.greedyHits + newPirate.greedyHits;
+
+          updatedPirates.push({
+            ...existingPirate,
+            greedyHits: updatedGreedyHits,
+            battles: existingPirate.battles + 1,
+          });
+        }
+      });
+
+      if (wonBattle) {
+        updatedWins += 1;
+      } else {
+        updatedLosses += 1;
+      }
+
+      updatedPOE += poe;
+      updatedCommodities += commodities;
+      updatedLavishLockers += lavishLockers;
+
+      updatedBattles.push({
+        wonBattle,
+        poe,
+        commodities,
+        lavishLockers,
+        pirates: newPirates,
+        greedyHitPayCommands,
+        greedyHitsSummary,
+      });
+
+      return {
+        ...prevState,
+        wins: updatedWins,
+        losses: updatedLosses,
+        poe: updatedPOE,
+        commodities: updatedCommodities,
+        lavishLockers: updatedLavishLockers,
+        pirates: updatedPirates,
+        battles: updatedBattles,
+      };
+    });
+  };
+
+  const deleteLastBattle = () => {
+    setResults((prevState) => {
+      if (prevState.battles.length === 0) {
+        return prevState;
+      }
+
+      const lastBattle = prevState.battles[prevState.battles.length - 1];
+      const updatedPirates = prevState.pirates.map((pirate) => {
+        const matchingPirateInBattle = lastBattle.pirates.find(
+          (battlePirate) => battlePirate.name === pirate.name
+        );
+        if (matchingPirateInBattle) {
+          const newGreedyHits =
+            pirate.greedyHits - matchingPirateInBattle.greedyHits;
+          const newBattlesCount = pirate.battles - 1;
+          return {
+            ...pirate,
+            greedyHits: newGreedyHits,
+            battles: newBattlesCount,
+          };
+        }
+        return pirate;
+      });
+
+      return {
+        ...prevState,
+        wins: lastBattle.wonBattle ? prevState.wins - 1 : prevState.wins,
+        losses: lastBattle.wonBattle ? prevState.losses : prevState.losses - 1,
+        poe: prevState.poe - lastBattle.poe,
+        commodities: prevState.commodities - lastBattle.commodities,
+        lavishLockers: prevState.lavishLockers - lastBattle.lavishLockers,
+        pirates: updatedPirates,
+        battles: prevState.battles.slice(0, -1), // Remove the last battle
+      };
+    });
   };
 
   const toggleExpandShipInfo = () => {
